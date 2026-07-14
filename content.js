85b52bc1c5ef160f39cf95128652070f4885ed72
devendrakumar92717-arch <devendrakumar92717@gmail.com>
2026-07-14 13:25:56 +0530

    Implement movie management functions
    Added functions to update, delete, and retrieve movies.

--- content.js
+++ content.js
@@ -56,3 +56,77 @@ function findMovie(movieId) {
 }
 
 console.log("Content Part 1 Ready");
+
+/* ======================================
+   BabyJohnHub OTT
+   Content Management
+   Part 2/6
+====================================== */
+
+// Update Movie
+
+function updateMovie(movieId, updatedData) {
+
+    let movies = getMovies();
+
+    movies = movies.map(movie =>
+
+        movie.id === movieId
+            ? { ...movie, ...updatedData }
+            : movie
+
+    );
+
+    saveMovies(movies);
+
+    console.log("Movie Updated");
+
+}
+
+// Delete Movie
+
+function deleteMovie(movieId) {
+
+    const movies = getMovies().filter(
+
+        movie => movie.id !== movieId
+
+    );
+
+    saveMovies(movies);
+
+    console.log("Movie Deleted");
+
+}
+
+// Featured Movies
+
+function getFeaturedMovies() {
+
+    return getMovies().filter(
+
+        movie => movie.featured === true
+
+    );
+
+}
+
+// Recently Added
+
+function getRecentlyAdded(limit = 10) {
+
+    return getMovies()
+
+        .sort((a, b) =>
+
+            new Date(b.createdAt) -
+
+            new Date(a.createdAt)
+
+        )
+
+        .slice(0, limit);
+
+}
+
+console.log("Content Part 2 Ready");
